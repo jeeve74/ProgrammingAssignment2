1745c0f04de46470ca8f124da1c61be17acb54b3
jeeve74 <mroqueni@statera.com>
2014-12-16 18:37:04 -0700

    converted 2 functions to support inverse matrix and added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions that are used to create a special object that stores a matrix
+##      and cache's its inverse.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a special "vector" which is really an object that:
+##      sets the value of the vector
+##      gets the value of the vector
+##      sets the value of the inverse
+##      gets the value of the inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(origMatrix = matrix()) {
+        invMatrix <- NULL
+        set <- function(y) {
+                origMatrix <<- y
+                invMatrix <<- NULL
+        }
+        get <- function() origMatrix
+        setSolve <- function(solve) invMatrix <<- solve
+        getSolve <- function() invMatrix
+        list(set = set, get = get,
+             setSolve = setSolve,
+             getSolve = getSolve)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve calculates the inverse of the special "vector" created with the above function. 
+##      If the inverse has already been calculated, then it gets the inverse from the cache 
+##      and skips the computation. Otherwise, it calculates the inverse
+##      and sets the value of the inverse in the cache via the setSolve function.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(origMatrix, ...) {
+        invMatrix <- origMatrix$getSolve()
+        if(!is.null(invMatrix)) {
+                message("getting cached data")
+                return(invMatrix)
+        }
+        data <- origMatrix$get()
+        invMatrix <- solve(data, ...)
+        origMatrix$setSolve(invMatrix)
+        invMatrix
 }
